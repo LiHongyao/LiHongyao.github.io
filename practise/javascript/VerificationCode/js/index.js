59a41f8f85c5c9725ab14a03b595a94a5e525d96
LiHy <lihy_online@163.com>
2018-05-31 19:57:26 +0800

    内容更新

--- practise/javascript/VerificationCode/js/index.js
+++ practise/javascript/VerificationCode/js/index.js
@@ -4,6 +4,7 @@
     // 获取存储验证码元素
     var validateCode = document.querySelector(".validate-code");
     var input = document.querySelector("input");
+    var inputBox = document.querySelector(".input-box");
     // 设置默认验证码
     validateCode.textContent = getVerificationCode(6);
     // 点击验证码/切换
@@ -14,10 +15,12 @@
     input.onblur = function() {
         // 根据验证码生成正则表达式
         // i 忽略大小写
-        var reg = new RegExp(validateCode.textContent, "i");
+        var reg = new RegExp(`^${validateCode.textContent}$`, "i");
         // 通过正则表达式判断验证码是否输入正确
         if(!reg.test(this.value)) {
-            alert("验证码不匹配！");
+            inputBox.classList.remove("hidden");
+        }else {
+            inputBox.classList.add("hidden");
         }
     }
  
