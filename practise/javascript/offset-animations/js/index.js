d60c8b0983df6bae891ca2fd74766606363fa4cd
LiHy <lihy_online@163.com>
2018-06-01 11:49:45 +0800

    内容更新

--- practise/javascript/offset-animations/js/index.js
+++ practise/javascript/offset-animations/js/index.js
@@ -1,13 +1,12 @@
-
 (function () {
-
-    var kHeight     = window.innerHeight,
-        offset      = 0,
-        items       = $.get(".list-item"),
-        offsetTops  = [];
+    var kHeight     = window.innerHeight, // 获取屏幕高度
+        offset      = 0, // 记录页面滚动的距离
+        items       = $.get(".list-item"), // 获取元素
+        offsetTops  = []; // 记录元素在页面中的位置
     for(var i = 0, len = items.length; i < len; i++) {
         offsetTops[i] = items[i].offsetTop;
     }
+    console.log(offsetTops);
 
     $.addEvent(window, "scroll", function () {
         offset = document.body.scrollTop || document.documentElement.scrollTop;
