f250018094a3e490a2d7afe08eba12d7313bd675
LiHy <lihy_online@163.com>
2018-06-07 18:36:21 +0800

    修改分页页码

--- practise/javascript/page/demo-2/js/index.js
+++ practise/javascript/page/demo-2/js/index.js
@@ -3,8 +3,8 @@
     var oContainer = document.querySelector("#page-box");
     var page = new LHYPage({
         container: oContainer,
-        curPage: 5,
-        allPage: 10,
+        curPage: 88,
+        allPage: 100,
         callBack: function(curPage) {
             console.log(curPage);
         }
