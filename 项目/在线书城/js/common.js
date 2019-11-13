a4acca0d2e463bccc3156f1151afe59bc84de09e
Li-HONGYAO <lihy_online@163.com>
2019-11-13 09:59:09 +0800

    内容更新

--- 项目/在线书城/js/common.js
+++
@@ -1,6 +0,0 @@
-$(function() {
-    // 返回首页
-    $(".goHome").on("click", () => {
-        location.href = "../index.html";
-    });
-});
--- 项目/多克科技/js/service-proj.js
+++
@@ -1,7 +0,0 @@
-
-let vm = new Vue({
-    el: "#content",
-    data: getData("serviceProj")
-});
-
-
