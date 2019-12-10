0c7d41e06d5a3783fa715fa3d5be5509b6543764
Li-HONGYAO <lihy_online@163.com>
2019-12-10 14:08:06 +0800

    内容更新

--- Practice/JavaScript/轮播图/js/fn.js
+++ Practice/JavaScript/轮播图/js/fn.js
@@ -4,7 +4,7 @@ function fade() {
     let paginationItems = [...document.querySelectorAll(".fade .pagination-item")];
     let prevBtn = document.querySelector(".fade .prev");
     let nextBtn = document.querySelector(".fade .next");
-    let flash   = document.querySelector(".fade .flash");
+    let flash = document.querySelector(".fade .flash");
     let curIndex = 0, // 记录当前下标
         lasIndex = 0, // 记录上一次下标
         length = slides.length, // 获取图片张数
@@ -16,7 +16,7 @@ function fade() {
     flash.onmouseenter = stop; // 当鼠标移至容器上时，暂停定时器
     flash.onmouseleave = play; // 当鼠标移出容器上时，启动定时器
     // 3.2. 点击下一页
-    nextBtn.onclick = function () { 
+    nextBtn.onclick = function () {
         // 计算下标
         curIndex = curIndex == length - 1 ? 0 : ++curIndex;
         // 切换视图
@@ -89,7 +89,7 @@ function scroll() {
     let length = paginationItems.length, // 获取图片显示的张数（实际显示）
         curIndex = 1, // 记录当前显示图片的下标
         lastSelIndex = 1; // 记录上一次显示图片的下标，便于后面移除
-        isAnimating = false, // 记录当前是否正在做一个滚动效果
+    isAnimating = false, // 记录当前是否正在做一个滚动效果
         timer = null; // 定时器
     // 2. 更新位置
     wrapper.style.transform = `translateX(-${width}px)`;
@@ -99,24 +99,24 @@ function scroll() {
     flash.onmouseenter = stop;
     flash.onmouseleave = play;
     window.onresize = function () {
-        width = parseInt(getStyle(flash, "width")); 
+        width = parseInt(getStyle(flash, "width"));
     }
     nextBtn.onclick = function () {
-        if(isAnimating) return;
+        if (isAnimating) return;
         curIndex = curIndex == length ? 1 : ++curIndex;
         tab(-width);
         updatePagination();
     }
     prevBtn.onclick = function () {
-        if(isAnimating) return;
+        if (isAnimating) return;
         curIndex = curIndex == 1 ? length : --curIndex;
         tab(width);
         updatePagination();
     }
     paginationItems.forEach(pagination => {
-        pagination.onclick = function() {
+        pagination.onclick = function () {
             let index = paginationItems.indexOf(this) + 1;
-            if(index == curIndex || isAnimating) return;
+            if (index == curIndex || isAnimating) return;
             let offset = -width * (index - curIndex);
             curIndex = index;
             tab(offset);
@@ -133,7 +133,6 @@ function scroll() {
         clearInterval(timer);
     }
     function tab(offset) {
-        // 计算目标偏移  
         let desOffset = lastSelIndex * -width + offset;
         let curOffset = lastSelIndex * -width;
         // 帧动画
@@ -157,15 +156,83 @@ function scroll() {
                     wrapper.style.transform = `translateX(${-length * width}px)`;
                 } else if (desOffset < -length * width) {
                     wrapper.style.transform = `translateX(${-width}px)`;
-                } 
+                }
             }
         }, interval);
     }
+
     function updatePagination() {
         paginationItems[lastSelIndex - 1].classList.remove("sel");
         paginationItems[curIndex - 1].classList.add("sel");
         lastSelIndex = curIndex;
     }
+    function getStyle(el, attr) {
+        if (el.currentStyle) {
+            return el.currentStyle[attr];
+        } else {
+            return getComputedStyle(el, null)[attr];
+        }
+    }
+}
+
+
+function trans() {
+    // 1. 获取元素
+    let prevBtn = document.querySelector(".trans .prev");
+    let nextBtn = document.querySelector(".trans .next");
+    let flash = document.querySelector(".trans .flash");
+    let wrapper = document.querySelector(".trans .wrapper");
+    // 2. 获取容器宽度并更新轮播图偏移显示实际的第1张图片
+    let width = parseInt(getStyle(flash, "width"));
+    wrapper.style.transform = `translateX(-${width}px)`;
+    // 3. 定义变量
+    let curIndex = 1, // 记录当前显示的下标
+        lastIndex = 1, // 记录上一次显示的下标
+        isAnimating = true; // 记录是否可以切换
+    // 4. 点击上一页
+    prevBtn.onclick = function () {
+        curIndex = curIndex == 0 ? 7 : --curIndex;
+        tab();
+    }
+    // 5. 点击下一页
+    nextBtn.onclick = function () {
+        curIndex = curIndex == 7 ? 0 : ++curIndex;
+        tab();
+    }
+    // 6. 点击小圆点
+    
+    function tab() {
+        if (!isAnimating) {
+            return;
+        }
+        isAnimating = false;
+        // 更新位置
+        wrapper.style.transform = `translateX(${curIndex * -width}px)`;
+        setTimeout(() => {
+            // 判断边界值
+            // curIndex < lastIndex 上一页 
+            // curIndex > lastIndex 下一页
+            if (curIndex < lastIndex && curIndex == 0 || curIndex > lastIndex && curIndex == 7) {
+                // 取消过渡
+                wrapper.style.transition = "none";
+                // 获取显示下标
+                let index = curIndex < lastIndex ? 6 : 1;
+                // 暗度陈仓，更新偏移
+                wrapper.style.transform = `translateX(${index * -width}px)`;
+                setTimeout(() => {
+                    wrapper.style.transition = "all .25s linear";
+                    curIndex = index;
+                    isAnimating = true;
+                }, 250);
+
+            } else {
+                isAnimating = true;
+                lastIndex = curIndex;
+            }
+            
+        }, 250);
+    }
+    // 获取非行内样式
     function getStyle(el, attr) {
         if (el.currentStyle) {
             return el.currentStyle[attr];
