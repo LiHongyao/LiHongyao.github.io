81856eb14dd02d81a8b970292074f23eaa60e7ef
Li-HONGYAO <lihy_online@163.com>
2019-12-17 20:20:14 +0800

    内容更新

--- Projects/React/成都生活展/client_test/src/util.js
+++ Projects/React/成都生活展/client_test/src/util.js
@@ -1,4 +1,5 @@
 // 工具
-export const BASE_URL = "http://10.2.0.150:8081";
+export const BASE_URL = "http://localhost:8081"
+// export const BASE_URL = "http://10.2.0.150:8081";
 // export const BASE_URL = "http://192.168.31.212:8081";
 
