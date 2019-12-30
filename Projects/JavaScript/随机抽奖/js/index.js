a3bba8cf2bb8992ca417a0909106b509cc7d6a83
Li-HONGYAO <lihy_online@163.com>
2019-12-30 23:28:17 +0800

    修改抽奖活动名单

--- Projects/JavaScript/随机抽奖/js/index.js
+++ Projects/JavaScript/随机抽奖/js/index.js
@@ -2,8 +2,14 @@ let btn = document.querySelector(".button");
 let res = document.querySelector(".res");
 let _isRunning = false;
 let _timer = null;
-let _source = [...NAMES];
+let _source = [];
 let _index = 0;
+// 名单集合
+for(let i = 1; i <= 50; i++) {
+    let num = i < 10 ? `0${i}` : i;
+    _source.push(`NO.000${num}`);
+}
+console.log(_source);
 btn.onclick = function() {
     _isRunning = !_isRunning;
     btn.textContent = _isRunning ? "停止抽奖" : "开始抽奖";
